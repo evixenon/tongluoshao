634ab00fad68c9b77f7dc34b2f591f77accb5700
evixenon <103761356@qq.com>
2022-06-19 00:21:13 +0200

    add tools; add decider v0.1

---
+++ tools/src/ToolApp.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Decider from './Decider';
+import EmptyTool from './EmptyTool';
+
+function ToolApp(props: {name: string;}): JSX.Element {
+    
+  function renderApp(): JSX.Element {
+    if (props.name === '') return <EmptyTool />;
+    switch (props.name) {
+        case 'decider':
+            return <Decider />
+        default:
+            return <EmptyTool />
+    }
+  }
+  
+  return (
+    <>
+        {renderApp()}
+    </>
+  )
+}
+
+export default ToolApp;
